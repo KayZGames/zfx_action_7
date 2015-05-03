e6f6d2982ef20f5dafe4fbd647ec24be67613c5f
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2015-05-03 16:01:49 +0200

    local files can be used for background music

--- lib/src/client/systems/sound.dart
+++ lib/src/client/systems/sound.dart
@@ -1,6 +1,7 @@
 part of client;
 
 class BackgroundMusicSystem extends VoidEntitySystem {
+  BeatFactorSystem bfs;
   AudioContext audioContext;
   AnalyserNode analyser;
   AudioBuffer kick;
@@ -8,6 +9,7 @@ class BackgroundMusicSystem extends VoidEntitySystem {
   AudioBuffer hihat;
   Uint8List byteFrequencyData;
   InputElement mute = querySelector('#mute');
+  InputElement musicFile = querySelector('#music');
 
   BackgroundMusicSystem(this.audioContext, this.byteFrequencyData);
 
@@ -18,16 +20,13 @@ class BackgroundMusicSystem extends VoidEntitySystem {
     var sounds = <Future<HttpRequest>>[];
     sounds.add(HttpRequest.request('packages/zfx_action_7/assets/sfx/237581__frankum__ambient-electro-loop.ogg',
         responseType: 'arraybuffer'));
-//    sounds.add(HttpRequest.request('packages/zfx_action_7/assets/sfx/misanthropy.ogg', responseType: 'arraybuffer'));
-//    sounds.add(HttpRequest.request('packages/zfx_action_7/assets/sfx/26903__vexst__snare-4.ogg', responseType: 'arraybuffer'));
-//    sounds.add(HttpRequest.request('packages/zfx_action_7/assets/sfx/26880__vexst__closed-hi-hat-2.ogg', responseType: 'arraybuffer'));
+    var source = audioContext.createBufferSource();
     Future.wait(sounds).then((requests) {
       var audioBuffers = <Future<AudioBuffer>>[];
       requests.forEach((request) {
         audioBuffers.add(audioContext.decodeAudioData(request.response));
       });
       Future.wait(audioBuffers).then((buffers) {
-        var source = audioContext.createBufferSource();
         source.buffer = buffers[0];
         if (!mute.checked) {
           source.connectNode(audioContext.destination);
@@ -44,6 +43,33 @@ class BackgroundMusicSystem extends VoidEntitySystem {
           }
         });
       });
+    });
+    musicFile.accept = "";
+    var formats = <String>[];
+    var audio = new AudioElement();
+    var goodAnswer = ['probably', 'maybe'];
+    if (goodAnswer.contains(audio.canPlayType('audio/ogg'))) {
+      formats.add('audio/ogg');
+    }
+    if (goodAnswer.contains(audio.canPlayType('audio/mp3'))) {
+      formats.add('audio/mp3');
+    }
+    musicFile.accept = formats.join(',');
+    musicFile.onChange.listen((data) {
+      var files = data.target.files as List<File>;
+      if (files.isNotEmpty) {
+        var reader = new FileReader();
+        reader.onLoadEnd.listen((event) {
+          if (reader.readyState == FileReader.DONE) {
+            audioContext.decodeAudioData(reader.result.buffer).then((audioBuffer) {
+              source.buffer = audioBuffer;
+              bfs.maxBeat = 1.0;
+            });
+          }
+        });
+
+        reader.readAsArrayBuffer(files[0].slice(0, files[0].size));
+      }
     });
   }
 
